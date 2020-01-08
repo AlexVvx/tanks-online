a61b826b7573a39c9581c9f40f4af0573b4c8671
AlexVvx <alex.vinober@gmail.com>
2020-01-08 14:16:50 -0500

    added test websocket server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const WebSocket = require('ws');
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +8,15 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const wss = new WebSocket.Server({ port: 8080 });
+console.log('starting websocket')
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
