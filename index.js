8facf7bd813e00f7440306ccecf163cc3936a167
AlexVvx <alex.vinober@gmail.com>
2020-01-08 14:25:54 -0500

    testing port 80

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
 const WebSocket = require('ws');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+const wss = new WebSocket.Server({ port: PORT });
+console.log('starting websocket')
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
